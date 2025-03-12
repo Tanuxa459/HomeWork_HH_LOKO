31f906719763675fbcfdfe8cdd3d9e3e9cf22870
Tanya <kliminatatianayog@gmail.com>
2025-03-12 13:30:52 +0300

    Merge remote-tracking branch 'origin/main'

--- src/test/java/tests/LokoBankPageObject.java
+++ src/test/java/tests/LokoBankPageObject.java
@@ -12,9 +12,10 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.logevents.SelenideLogger.step;
 
+@Tag("Loko")
 public class LokoBankPageObject extends TestBase {
 
-    @Tag("Loko")
+
     @Test
     void checkFormForCredit() {
         MainPage mainPage = new MainPage();
